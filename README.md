8f72666caf443d67672b6259ba2ecd2784826b80
Congzheng Song <csong4@apple.com>
2022-04-04 16:54:49 -0700

    initial dataset release

---
+++ README.md
@@ -0,0 +1,105 @@
+# Federated Learning Annotated Image Repository (FLAIR): A large labelled image dataset for benchmarking in federated learning
+
+FLAIR is a large dataset of images that captures a number of characteristics encountered in federated learning and privacy-preserving ML tasks. 
+This dataset comprises approximately 430,000 images from 51,000 Flickr users, which will better reflect federated learning problems arising in practice, and it is being released to aid research in the field.
+
+## Image Labels
+These images have been annotated by humans and assigned labels from a taxonomy of more than 1,600 fine-grained labels. 
+All main subjects present in the images have been labeled, so images may have multiple labels. 
+The taxonomy is hierarchical where the fine-grained labels can be mapped to 17 coarse-grained categories.
+The dataset includes both fine-grained and coarse-grained labels so researchers can vary the complexity of a machine learning task.
+
+## User Labels and their use for Federated Learning
+We have used image metadata to extract artist names/IDs for the purposes of creating user datasets for federated learning. 
+While optimization algorithms for machine learning are often designed under the assumption that each example is an independent sample from the distribution, federated learning applications deviate from this assumption in a few different ways that are reflected in our user-annotated examples. 
+Different users differ in the number of images they have, as well as the number of classes represented in their image collection. 
+Further, images of the same class but taken by different users are likely to have some distribution shift. 
+These properties of the dataset better reflect federated learning applications, and we expect that benchmark tasks on this dataset will benefit from algorithms designed to handle such data heterogeneity.
+
+## Getting Started
+### Prerequisites
+Please make sure you have python >= 3.6 and have the required packages installed (see below).
+```sh
+python3 -m pip install -r requirements.txt
+```
+### Download the dataset
+Ensure you have a good network connection to download the ~6GB of image data, and enough local space to store and decompress it.
+Download the dataset with the following command:
+```sh
+python3 download_dataset.py --dataset_dir=/path/to/data 
+```
+The images and metadata will be saved to the provided `dataset_dir`.
+By default the script will download the down-sized images (size = 256 x 256). 
+The images are split and compressed into dozens of tarball archives and will be decompressed after downloading.
+If you wish to download the full-size raw images, add `--download_raw` flag in the above command.\
+ ⚠️ Warning: the raw images take up to ~1.2TB disk space to store after decompressing.
+
+After downloading and decompressing, the `dataset_dir` will have the following layout:
+```
+dataset_dir
+├── labels_and_metadata.json      # a list of labels and metadata for each image
+├── label_relationship.txt        # a list of `(fine-grained label, label)` pair
+├── small_images
+│   └── *.jpg                     # all down-sized images
+└── raw_images                    # exists if you added `--download_raw` flag
+    └── *.jpg                     # all raw images
+```
+
+### Dataset split
+We include a standard train/val/test split in `labels_and_metadata.json`.
+The partition is based on user ids with ratio 8:1:1, i.e. train, val and test sets have disjoint users.
+Below are the numbers for each partition:
+
+| Partition        | Train   | Val    | Test   |
+| ---------------- | ------- | ------ | ------ |
+| Number of users  | 41,131  | 5,141  | 5,142  |
+| Number of images | 345,879 | 39,239 | 43,960 |
+
+We recommend using the provided split for reproducible benchmarks.
+
+### Explore the dataset
+Below is an example metadata and label for one image from `labels_and_metadata.json`:
+```json
+{
+    "user_id": "59769174@N00",
+    "image_id": "14913474848",
+    "fine_grained_labels": [
+        "bag",
+        "document",
+        "furniture",
+        "material",
+        "printed_page"
+    ],
+    "labels": [
+        "equipment",
+        "material",
+        "structure"
+    ],
+    "partition": "train"
+}
+```
+Field `image_id` is the Flickr PhotoID and `user_id` is the Flickr NSID that owns the image.
+Field `partition` denotes which `train/dev/test` partition the image belongs to.
+Field `fine_grained_labels` is a list of annotated labels presenting the subjects in the image and `labels` is the list of coarse-grained labels obtained by  mapping fine-grained labels to higher-order categories.
+The file `label_relationship.txt` includes the mapping from ~1,600 fine-grained labels to 17 higher-order categories.
+
+We provide scripts to explore the images and labels in more detail. First you need to start a jupyter notebook:
+```sh
+jupyter notebook
+```
+- To explore the downloaded images, open in jupyter notebook [`explore_images.ipynb`](./explore_images.ipynb) which displays the images with corresponding metadata and labels.
+- To explore the labels, open in jupyter notebook  [`explore_labels.ipynb`](./explore_labels.ipynb) which displays the statistics of the user and label distribution.
+
+### Prepare the dataset
+We provide a script to prepare the dataset in HDF5 format for more efficient processing and training:
+```sh
+python3 prepare_dataset.py --dataset_dir=/path/to/data --output_file=/path/to/hdf5
+```
+By default the script will group the images and labels by train/val/test split and then by user ids, making it suitable for federated learning experiments.
+With the flag `--not_group_data_by_user`, the script will simply group the images and labels by train/val/test split and ignore the user ids, which is the typical setup for centralized training. \
+⚠️ Warning: the hdf5 file take up to ~80GB disk space to store after processing.
+
+## Disclaimer
+The annotations and Apple’s other rights in the dataset are licensed under CC-BY-NC 4.0 license. 
+The images are copyright of the respective owners, the license terms of which can be found using the links provided in ATTRIBUTIONS.TXT (by matching the Image ID). 
+Apple makes no representations or warranties regarding the license status of each image and you should verify the license for each image yourself.
---
+++ prepare_dataset.py
@@ -0,0 +1,200 @@
+# -*- coding: utf-8 -*-
+#
+# For licensing see accompanying LICENSE file.
+# Copyright (C) 2022 Apple Inc. All Rights Reserved.
+
+import argparse
+import os
+import sys
+import logging
+
+import h5py
+import json
+import numpy as np
+
+from typing import Dict, List
+from collections import defaultdict, Counter
+from PIL import Image
+
+
+logger = logging.getLogger(name=__name__)
+
+LABEL_DELIMITER = '|'   # Labels will be joined by delimiter and saved to hdf5
+LOG_INTERVAL = 500  # Log the preprocessing progress every interval steps
+
+
+def load_user_metadata(labels_file: str) -> Dict[str, List]:
+    """
+    Load labels and metadata keyed by `user_id`.
+
+    :param labels_file:
+        A .json file with a list of labels and metadata dictionaries. Each
+        dictionary has keys: `[image_id,user_id,labels,fine_grained_labels]`.
+        * `image_id` is the ID of an image.
+        * `user_id` is the ID of the user `image_id` belongs to.
+        * `labels` is a list of 17 higher-order class labels.
+        * `fine_grained_labels` is a list of 1,628 fine-grained class labels.
+    :return:
+        A dictionary where key is `user_id` and value is a list of labels and
+        metadata for each image `user_id` owns.
+    """
+    user_metadata = defaultdict(list)
+    with open(labels_file) as f:
+        metadata_list = json.load(f)
+
+    for metadata in metadata_list:
+        user_metadata[metadata["user_id"]].append(metadata)
+    return user_metadata
+
+
+def preprocess_federated_dataset(input_data_dir: str,
+                                 labels_file: str,
+                                 output_file: str):
+    """
+    Process images and labels into a HDF5 federated dataset where data is
+    first split by train/test partitions and then split again by user ID.
+
+    :param input_data_dir:
+        Path to directory of images output from the script
+        `download_dataset.sh`.
+    :param labels_file:
+        A .json file with a list of labels and metadata dictionaries. Each
+        dictionary has keys: `[image_id,user_id,labels,fine_grained_labels]`.
+        * `image_id` is the ID of an image.
+        * `user_id` is the ID of the user `image_id` belongs to.
+        * `labels` is a list of 17 higher-order class labels.
+        * `fine_grained_labels` is a list of ~1,600 fine-grained class labels.
+    :param output_file:
+        Output path for HDF5 file. Use the postfix `.hdf5`.
+    """
+    logger.info('Preprocessing federated dataset.')
+    user_metadata = load_user_metadata(labels_file)
+    label_counter = Counter()
+    fine_grained_label_counter = Counter()
+    with h5py.File(output_file, 'w') as h5file:
+        # Iterate through users of each partition.
+        for i, user_id in enumerate(user_metadata):
+            # Load and concatenate all images of a user.
+            image_array, image_id_array = [], []
+            labels_array, fine_grained_labels_array = [], []
+            # Load and concatenate all images and labels of a user.
+            for metadata in user_metadata[user_id]:
+                image_id = metadata["image_id"]
+                image = Image.open(
+                    os.path.join(input_data_dir, f"{image_id}.jpg"))
+                image_array.append(np.asarray(image))
+                image_id_array.append(image_id)
+                # Encode labels as a single string, separated by delimiter |
+                labels_array.append(LABEL_DELIMITER.join(metadata["labels"]))
+                fine_grained_labels_array.append(
+                    LABEL_DELIMITER.join(metadata["fine_grained_labels"]))
+                # Update label counter
+                label_counter.update(metadata["labels"])
+                fine_grained_label_counter.update(
+                    metadata["fine_grained_labels"])
+
+            partition = user_metadata[user_id][0]["partition"]
+            # Multiple variable-length labels. Needs to be stored as a string.
+            h5file[f'/{partition}/{user_id}/labels'] = np.asarray(
+                labels_array, dtype='S')
+            h5file[f'/{partition}/{user_id}/fine_grained_labels'] = np.asarray(
+                fine_grained_labels_array, dtype='S')
+            h5file[f'/{partition}/{user_id}/image_ids'] = np.asarray(
+                image_id_array, dtype='S')
+            # Tensor with dimensions [num_images,width,height,channels]
+            h5file.create_dataset(
+                f'/{partition}/{user_id}/images', data=np.stack(image_array))
+
+            if (i + 1) % LOG_INTERVAL == 0:
+                logger.info("Processed {0}/{1} users".format(
+                    i + 1, len(user_metadata)))
+
+        # Write metadata
+        h5file['/metadata/label_counter'] = json.dumps(label_counter)
+        h5file['/metadata/fine_grained_label_counter'] = json.dumps(
+            fine_grained_label_counter)
+
+    logger.info('Finished preprocess federated dataset successfully!')
+
+
+def preprocess_central_dataset(input_data_dir: str,
+                               labels_file: str,
+                               output_file: str):
+    """
+    Process images and labels into a HDF5 (not federated) dataset where
+    data is split by train/val/test partitions.
+
+    Same parameters as `preprocess_federated_dataset`.
+    """
+    logger.info('Preprocessing central dataset.')
+    user_metadata = load_user_metadata(labels_file)
+    label_counter = Counter()
+    fine_grained_label_counter = Counter()
+    with h5py.File(output_file, 'w') as h5file:
+        # Iterate through users of each partition.
+        for i, user_id in enumerate(user_metadata):
+            # Load and concatenate all images of a user.
+            for metadata in user_metadata[user_id]:
+                image_id = metadata["image_id"]
+                image = Image.open(
+                    os.path.join(input_data_dir, f"{image_id}.jpg"))
+                partition = metadata["partition"]
+                h5file.create_dataset(
+                    f'/{partition}/{image_id}/image', data=np.asarray(image))
+                # Encode labels as a single string, separated by delimiter |
+                h5file[f'/{partition}/{image_id}/labels'] = LABEL_DELIMITER.join(
+                    metadata["labels"])
+                h5file[f'/{partition}/{image_id}/fine_grained_labels'] = (
+                    LABEL_DELIMITER.join(metadata["fine_grained_labels"]))
+                h5file[f'/{partition}/{image_id}/user_id'] = user_id
+                # Update label counter
+                label_counter.update(metadata["labels"])
+                fine_grained_label_counter.update(
+                    metadata["fine_grained_labels"])
+
+            if (i + 1) % LOG_INTERVAL == 0:
+                logger.info("Processed {0}/{1} users".format(
+                    i + 1, len(user_metadata)))
+
+        # Write metadata
+        h5file['/metadata/label_counter'] = json.dumps(label_counter)
+        h5file['/metadata/fine_grained_label_counter'] = json.dumps(
+            fine_grained_label_counter)
+
+    logger.info('Finished preprocessing central dataset successfully!')
+
+
+if __name__ == '__main__':
+    logging.basicConfig(
+        stream=sys.stdout,
+        level=logging.INFO,
+        format='%(asctime)s %(levelname)s: %(message)s')
+
+    argument_parser = argparse.ArgumentParser(
+        description=
+        'Preprocess the images and labels of FLAIR dataset into HDF5 files.')
+    argument_parser.add_argument(
+        '--dataset_dir',
+        required=True,
+        help='Path to directory of images and label file. '
+             'Can be downloaded using download_dataset.py')
+    argument_parser.add_argument(
+        '--output_file',
+        required=True,
+        help='Path to output HDF5 file that will be constructed by this script'
+    )
+    argument_parser.add_argument(
+        '--not_group_data_by_user',
+        action='store_true',
+        default=False,
+        help='If true, do not group data by user IDs.'
+             'If false, group data by user IDs to '
+             'make suitable for federated learning.')
+    arguments = argument_parser.parse_args()
+
+    image_dir = os.path.join(arguments.dataset_dir, "small_images")
+    labels_file = os.path.join(arguments.dataset_dir, "labels_and_metadata.json")
+    if arguments.not_group_data_by_user:
+        preprocess_central_dataset(image_dir, labels_file, arguments.output_file)
+    else:
+        preprocess_federated_dataset(image_dir, labels_file, arguments.output_file)
